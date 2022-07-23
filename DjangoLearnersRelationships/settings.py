16faebfef3a8c01c4a3af07db6f7c13f578012b3
A.K <akcoding2020@gmail.com>
2022-07-23 08:40:31 -0400

    Initial commit of reference projects

---
+++ DjangoLearnersRelationships/settings.py
@@ -0,0 +1,20 @@
+# Mariadb
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'sql_mode': 'traditional'
+        },
+        'NAME': 'db',
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
+
+INSTALLED_APPS = (
+    'related_objects',
+)
+
+SECRET_KEY = 'SECRET KEY for this Django Project'
---
+++ DjangoSimpleModelLab/settings.py
@@ -0,0 +1,16 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db',
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
+
+INSTALLED_APPS = (
+    'orm',
+)
+
+SECRET_KEY = 'sfdagdfghfghfhryhtrsadfsdawerhjdukfgfbjhgyukfyuryswergfdgsdfhfdghfdjgdjghfjythrtsyh'
